e734a55bdef1feb943058477deef05c7faa98a09
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-04-03 21:17:17 +0530

    MongoDB setup completed
    Basic Backend setup

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const app = require("./app.js");
+const connectDatabase = require("./db/Database.js");
+
+// Handle uncaught Exception
+process.on("uncaughtException", (err) => {
+    console.log(`unCaughtException Error: ${err.message}`);
+    console.log("Stopping server for unCaughtException Error...");
+});
+
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({
+        path: "./config/.env",
+    });
+}
+
+// Database
+connectDatabase();
+
+// Server
+const PORT = process.env.PORT;
+const server = app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+    console.log(`unhandledRejection Error..., ${err.message}`);
+    console.log("Stopping server for unhandledRejection Error...");
+
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
+// On Backend Call at "/"
+app.get("/", (req, res) => {
+    res.send("CERTYSTORE API");
+});
