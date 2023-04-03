78915e493914ee5f0fac1e27ed03760dc27c222b
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-04-03 17:23:07 +0530

    Login UI page completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import {
+  LoginPage,
+  SignupPage
+} from "./Routes";
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import styles from "../../styles/styles";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold font-Poppins text-gray-900">
+          Login to your Account
+        </h2>
+      </div>
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md font-Roboto">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form className="space-y-6">
+
+            {/* EMAIL INPUT FIELD */}
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                Your Email
+              </label>
+              <div className="mt-1">
+                <input
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 
+                  focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                  type="email"
+                  name="email"
+                  value={email}
+                  placeholder="Enter Email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  autoComplete="email"
+                  required
+                />
+              </div>
+            </div>
+            {/* PASSWORD INPUT FIELD */}
+            <div>
+              <label htmlFor="email" className="block text-sm select-none font-medium text-gray-700">
+                Your Password
+              </label>
+              <div className="mt-1 relative">
+                <input
+                  className="appearance-none block w-full px-3 py-2 select-none border border-gray-300 rounded-md shadow-sm placeholder-gray-400 
+                  focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                  type={`${visible ? "text" : "password"}`}
+                  name="password"
+                  value={password}
+                  placeholder="Enter Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  autoComplete="current-password"
+                  required
+                />
+                {visible
+                  ? <AiOutlineEye
+                    className="absolute right-2 top-2 cursor-pointer"
+                    size={25}
+                    onClick={() => setVisible(false)}
+                  />
+                  : <AiOutlineEyeInvisible
+                    className="absolute right-2 top-2 cursor-pointer"
+                    size={25}
+                    onClick={() => setVisible(true)}
+                  />
+                }
+              </div>
+            </div>
+            {/* USER OPTIONS */}
+            <div className={`${styles.normalFlex} justify-between`}>
+              <div className={`${styles.normalFlex}`}>
+                <input
+                  className="text-blue-600 focus:ring-blue-500 border-gray-300 w-4 h-4 rounded"
+                  type="checkbox"
+                  name="remember-me"
+                  id="remember-me"
+                />
+                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                  Remember me
+                </label>
+              </div>
+              <div className="text-sm">
+                <Link to="/forgot" className="font-medium text-blue-600 hover:text-blue-500">
+                  Forgot your password?
+                </Link>
+              </div>
+            </div>
+            {/* LOGIN BUTTON */}
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full h-40px flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
+              >
+                LOGIN
+              </button>
+            </div>
+            {/* NEW USER */}
+            <div className={`${styles.normalFlex} w-full`}>
+              <h4>
+                New here?
+              </h4>
+              <Link to="/signup" className="text-blue-600 pl-2">
+                Create an account
+              </Link>
+            </div>
+
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
