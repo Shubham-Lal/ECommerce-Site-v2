35c57f06bedeef93b2394041ca1017b82f7ca5b9
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-04-08 18:19:54 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -19,13 +19,6 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
     });
 }
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Set-Cookie: cross-site-cookie=whatever; SameSite=None; Secure");
-    next();
-});
-
 const corsOptions = {
     origin: process.env.FRONTEND_URL,
     credentials: true,
